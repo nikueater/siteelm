a178c99071783024d15ab08e67ed94d5cd7abff8
nikueater <nikueater@outlook.com>
2019-09-27 15:52:54 +0900

    read external yaml

--- src/generator.ts
+++ src/generator.ts
@@ -2,7 +2,9 @@ import fs from 'fs'
 import {JSDOM} from 'jsdom'
 import P from 'parsimmon'
 import yaml from 'js-yaml'
+import path from 'path'
 import {minify} from 'html-minifier'
+import traverse from 'traverse'
 
 interface Preamble {
     module: string
@@ -28,7 +30,7 @@ const generatePageFrom = (source: string, elmcode: string, withDraft: boolean): 
     try {
         // create flags
         const document = parseDocument(fs.readFileSync(source, 'utf-8'))
-        const p = parsePreamble(document[0])
+        const p = parsePreamble(document[0], source)
         const flags = {
             preamble: JSON.stringify(p),
             body: document[1]
@@ -81,9 +83,10 @@ const parseDocument = (source: string): string[] => {
     
 /** 
  * @param p yaml format string
+ * @param source path of the current source file
  * @returns preamble interface data
  */
-const parsePreamble = (p: string): Preamble => {
+const parsePreamble = (p: string, source: string): Preamble => {
     const yml = yaml.safeLoad(p)
     const preamble = ((x: any): Preamble => x)(yml)
     if(typeof preamble.module !== 'string') {
@@ -92,6 +95,24 @@ const parsePreamble = (p: string): Preamble => {
     if(typeof preamble.draft !== 'boolean') {
         preamble.draft = false
     }
+    // walk through all element to detect special values
+    traverse(preamble).forEach(function(x) {
+        switch(this.key) {
+            case 'external':
+                const dir = path.dirname(source)
+                const file = x || ''
+                const y = fs.readFileSync(path.normalize(path.join(dir, file)), 'utf-8')
+                const value = yaml.safeLoad(y)
+                if(this.parent) {
+                    if(Object.keys(this.parent.node).length === 1) {
+                        this.parent.update(value)
+                    } else {
+                        throw new InvalidPreambleError('"external" cannot have siblings')      
+                    }
+                } 
+                break
+        }
+    })
     return preamble
 }
 
