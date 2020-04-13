b12233b22a2e29c835eaffb228de9b410de6d308
nikueater <nikueater@outlook.com>
2020-04-13 21:15:01 +0900

    v0.2.12

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import generateAll, {copyAssets} from './generator'
 import initialize from './initializer'
 import watchAll from './watcher'
 
-const version = '0.2.11'
+const version = '0.2.12'
 
 program
     .version(version, '-v, --version')
